d2a2ca1383695d3d504c227e93fad8b05847b0dc
SPACETON💎 <143197697+spacetontoken@users.noreply.github.com>
2024-12-29 01:30:15 +0300

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,6 +1,6 @@
 import { Bot } from "grammy";
 
-const bot = new Bot(process.env.TELEGRAM_BOT_TOKEN);
+const bot = new Bot(7399517799:AAExB_6fuB_SIYaGEzptfO3hUhCze2EHHzE);
 
 // Map is used for simplicity. For production use a database
 const paidUsers = new Map();
